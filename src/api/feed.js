c6b53292d74596e1265933f24afaf3edcb249985
Andre William <andrecool.cool@gmail.com>
2022-01-21 21:33:53 +0700

    refac: refactor fetching state (loading) and tag

--- src/api/feed.js
+++ src/api/feed.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { config } from '../config';
 
-export function getFeed() {
-  return axios.get(`${config.SERVER_ADD}/api/feeds`);
+export function getFeed(query) {
+  let { id = '', tags = [] } = query;
+  let search = new URLSearchParams({ id, tags });
+  return axios.get(`${config.SERVER_ADD}/api/feeds`, { params: search });
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,21 +9,28 @@ import ImageCard from '../../components/ImageCard/ImageCard';
 const Home = () => {
   const [image, setImage] = useState([]);
   const [tag, setTag] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
-      const feed = await getFeed();
+      const feed = await getFeed({ tag });
       setImage(feed.data.data);
+      setIsLoading(false);
     })();
-  }, []);
+  }, [tag]);
 
   return (
     <>
       <Searchbar />
       <ImageContainer>
-        {image.map((img, idx) => {
-          return <ImageCard key={idx} img={img} />;
-        })}
+        {!isLoading && image.length < 1 && <h1 className="text-6xl text-center mx-auto mt-32 text-white">Image not found</h1>}
+        {isLoading ? (
+          <h1 className="text-6xl text-center mx-auto mt-32 text-white">Loading</h1>
+        ) : (
+          image.map((img, idx) => {
+            return <ImageCard key={idx} img={img} />;
+          })
+        )}
       </ImageContainer>
     </>
   );
